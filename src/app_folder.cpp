c81668b324fa16604d4c85d7b0815aece90e23bb
William Yang <williamyang2017@gmail.com>
2022-02-07 23:57:27 +1100

    Switched to dx11 renderer and some refactoring

---
+++ src/app_folder.cpp
@@ -0,0 +1,252 @@
+#include <iostream>
+#include <filesystem>
+#include <string>
+#include <vector>
+#include <mutex>
+#include <thread>
+#include <functional>
+#include <ranges>
+#include <memory>
+
+#include <spdlog/spdlog.h>
+#include <fmt/core.h>
+#include "app.h"
+#include "app_credentials_schema.h"
+#include "tvdb_api.h"
+#include "tvdb_api_schema.h"
+#include "se_regex.h"
+#include "scanner.h"
+#include "file_loading.h"
+#include "renamer.h"
+
+#define EPISODES_CACHE_FN   "episodes.json"
+#define SERIES_CACHE_FN     "series.json"
+
+// include the windows libraries here because the macros interfere with our other headers
+#define WIN32_LEAN_AND_MEAN
+#define NOMINMAX
+#define NOGDI
+#include <windows.h>
+#include <shellapi.h>
+
+namespace app 
+{
+
+namespace fs = std::filesystem;
+
+class ScopedAtomic 
+{
+public:
+    std::atomic<bool> &m_var;
+    std::atomic<int> &m_busy_count;
+    ScopedAtomic(std::atomic<bool> &var, std::atomic<int> &busy_count)
+    : m_var(var), m_busy_count(busy_count) {
+        m_var = true;
+        m_busy_count++;
+    }
+
+    ~ScopedAtomic() {
+        m_var = false;
+        m_busy_count--;
+    }
+};
+
+SeriesFolder::SeriesFolder(
+    const fs::path &path, 
+    RenamingConfig &cfg,
+    std::atomic<int> &busy_count) 
+: m_path(path), m_cfg(cfg), m_global_busy_count(busy_count) 
+{
+    m_is_info_cached = false;
+    m_is_busy = false;
+    m_status = SeriesFolder::Status::UNKNOWN;
+    m_state = std::move(std::make_unique<ManagedFolder>());
+}
+
+// thread safe push of error to list
+void SeriesFolder::push_error(const std::string &str) {
+    std::scoped_lock lock(m_errors_mutex);
+    m_errors.push_back(str);
+}
+
+// thread safe load series search results from tvdb with validation
+bool SeriesFolder::load_search_series_from_tvdb(const char *name, const char *token) {
+    if (m_is_busy) return false;
+
+    auto scoped_hold = ScopedAtomic(m_is_busy, m_global_busy_count);
+
+    try {
+        auto search_opt = tvdb_api::search_series(name, token);
+        if (!search_opt) {
+            push_error(std::string("Failed to get search results from tvdb"));
+            return false;
+        }
+
+        auto search_doc = std::move(search_opt.value());
+        auto result = load_search_info(search_doc);
+        {
+            std::scoped_lock lock(m_search_mutex);
+            m_search_result = std::move(result);
+        }
+    } catch (std::exception &ex) {
+        push_error(std::string(ex.what()));
+        return false;
+    }
+    return false;
+}
+
+// thread safe load series and episodes data from tvdb with validation, and store as cache
+bool SeriesFolder::load_cache_from_tvdb(uint32_t id, const char *token) {
+    if (m_is_busy) return false;
+    auto scoped_hold = ScopedAtomic(m_is_busy, m_global_busy_count);
+
+    try {
+        auto series_opt = tvdb_api::get_series(id, token);
+        if (!series_opt) {
+            push_error(std::string("Failed to fetch series info from tvdb"));
+            return false;
+        }
+
+        auto episodes_opt = tvdb_api::get_series_episodes(id, token);
+        if (!episodes_opt) {
+            push_error(std::string("Failed to fetch episodes info from tvdb"));
+            return false;
+        }
+
+        auto series_doc = std::move(series_opt.value());
+        auto episodes_doc = std::move(episodes_opt.value());
+        auto series_cache = load_series_info(series_doc);
+        auto episodes_cache = load_series_episodes_info(episodes_doc);
+        // update cache
+        {
+            std::scoped_lock lock(m_cache_mutex);
+            TVDB_Cache cache = { series_cache, episodes_cache };
+            m_cache = std::move(cache);
+            m_is_info_cached = true;
+        }
+
+        auto series_cache_path = fs::absolute(m_path / SERIES_CACHE_FN);
+        if (!write_document_to_file(series_cache_path.string().c_str(), series_doc)) {
+            std::scoped_lock lock(m_errors_mutex);
+            push_error(std::string("Failed to write series cache file"));
+            return false;
+        }
+
+        auto episodes_cache_path = fs::absolute(m_path / EPISODES_CACHE_FN);
+        if (!write_document_to_file(episodes_cache_path.string().c_str(), episodes_doc)) {
+            std::scoped_lock lock(m_errors_mutex);
+            push_error(std::string("Failed to write series cache file"));
+            return false;
+        }
+    } catch (std::exception &ex) {
+        push_error(std::string(ex.what()));
+        return false;
+    }
+
+    return true;
+}
+
+// thread safe load the cache from local file (series.json, episodes.json)
+bool SeriesFolder::load_cache_from_file() {
+    if (m_is_busy) return false;
+    auto scoped_hold = ScopedAtomic(m_is_busy, m_global_busy_count);
+
+    const fs::path series_cache_fn = m_path / SERIES_CACHE_FN;
+    auto series_res = load_document_from_file(series_cache_fn.string().c_str());
+    if (series_res.code != DocumentLoadCode::OK) {
+        return false;
+    }
+
+    const fs::path episodes_cache_fn = m_path / EPISODES_CACHE_FN;
+    auto episodes_res = load_document_from_file(episodes_cache_fn.string().c_str());
+    if (episodes_res.code != DocumentLoadCode::OK) {
+        return false;
+    }
+
+    auto series_doc = std::move(series_res.doc);
+    auto episodes_doc = std::move(episodes_res.doc);
+
+    if (!validate_document(series_doc, tvdb_api::SERIES_DATA_SCHEMA)) {
+        push_error(std::string("Failed to validate series data"));
+        return false;
+    }
+
+    if (!validate_document(episodes_doc, tvdb_api::EPISODES_DATA_SCHEMA)) {
+        push_error(std::string("Failed to validate episodes data"));
+        return false;
+    }
+
+    auto series_cache = load_series_info(series_doc);
+    auto episodes_cache = load_series_episodes_info(episodes_doc);
+
+    {
+        std::scoped_lock lock(m_cache_mutex);
+        TVDB_Cache cache = { series_cache, episodes_cache };
+        m_cache = std::move(cache);
+        m_is_info_cached = true;
+    }
+    return true;
+}
+
+// update folder diff after cache has been loaded
+void SeriesFolder::update_state_from_cache() {
+    if (m_is_busy) return;
+    if (!m_is_info_cached && !load_cache_from_file()) {
+        return;
+    }
+
+    auto scoped_hold = ScopedAtomic(m_is_busy, m_global_busy_count);
+    auto intents = scan_directory(m_path, m_cache, m_cfg);
+
+    auto new_state = std::make_unique<ManagedFolder>();
+    for (auto &intent: intents) {
+        new_state->AddIntent(intent);
+    }
+
+    auto &counts = new_state->GetActionCount();
+    auto &conflict_table = new_state->GetConflicts();
+
+    if (counts.deletes > 0) {
+        m_status = Status::PENDING_DELETES;
+    } else if (conflict_table.size() > 0) {
+        m_status = Status::CONFLICTS;
+    } else if ((counts.renames > 0) || (counts.ignores > 0)) {
+        m_status = Status::PENDING_RENAME;
+    } else if (counts.completes > 0) {
+        m_status = Status::COMPLETED;
+    } else {
+        m_status = Status::EMPTY;
+    }
+
+    std::scoped_lock lock(m_state_mutex);
+    m_state = std::move(new_state);
+}
+
+// thread safe execute all actions in folder diff
+bool SeriesFolder::execute_actions() {
+    if (m_is_busy) return false;
+    auto scoped_hold = ScopedAtomic(m_is_busy, m_global_busy_count);
+
+    auto intents = m_state->GetIntents() | std::views::values | std::views::transform([](auto &intent) {
+        return std::reference_wrapper(intent.GetUnmanagedIntent());
+    });
+
+    std::scoped_lock lock(m_state_mutex);
+    return rename_series_directory(m_path, intents);
+}
+
+// execute the shell command to open the folder or file
+void SeriesFolder::open_folder(const std::string &path) {
+    auto filepath = m_path / path;
+    auto parent_dir = filepath.remove_filename();
+    auto parent_dir_str = parent_dir.string();
+    ShellExecuteA(NULL, "open", parent_dir_str.c_str(), NULL, NULL, SW_SHOW);
+}
+
+void SeriesFolder::open_file(const std::string &path) {
+    auto filepath = m_path / path;
+    auto filepath_str = filepath.string();
+    ShellExecuteA(NULL, "open", filepath_str.c_str(), NULL, NULL, SW_SHOW);
+}
+
+}
--- src/main_gui.cpp
+++ src/main_gui.cpp
@@ -1,17 +1,12 @@
-// Dear ImGui: standalone example application for GLFW + OpenGL 3, using programmable pipeline
-// (GLFW is a cross-platform general purpose library for handling windows, inputs, OpenGL/Vulkan/Metal graphics context creation, etc.)
+// Dear ImGui: standalone example application for DirectX 11
 // If you are new to Dear ImGui, read documentation from the docs/ folder + read the top of imgui.cpp.
 // Read online: https://github.com/ocornut/imgui/tree/master/docs
 
-#include <imgui.h>
-#include <imgui_impl_glfw.h>
-#include <imgui_impl_opengl3.h>
-#include <stdio.h>
-#if defined(IMGUI_IMPL_OPENGL_ES2)
-#include <GLES2/gl2.h>
-#endif
-#include <GLFW/glfw3.h> // Will drag system OpenGL headers
+#include "imgui.h"
+#include "imgui_impl_win32.h"
+#include "imgui_impl_dx11.h"
 
+#include <stdio.h>
 #include <iostream>
 #include <filesystem>
 
@@ -23,25 +18,35 @@
 #include "gui_ext.h"
 #include "font_awesome_definitions.h"
 
-// [Win32] Our example includes a copy of glfw3.lib pre-compiled with VS2010 to maximize ease of testing and compatibility with old VS compilers.
-// To link with VS2010-era libraries, VS2015+ requires linking with legacy_stdio_definitions.lib, which we do using this pragma.
-// Your own project should not be affected, as you are likely to link with a newer binary of GLFW that is adequate for your version of Visual Studio.
-#if defined(_MSC_VER) && (_MSC_VER >= 1900) && !defined(IMGUI_DISABLE_WIN32_FUNCTIONS)
-#pragma comment(lib, "legacy_stdio_definitions")
+#define WIN32_LEAN_AND_MEAN
+#define NOMINMAX
+#include <windows.h>
+#include <d3d11.h>
+#include <tchar.h>
+
+#ifdef NDEBUG
+#pragma comment(linker, "/SUBSYSTEM:windows /ENTRY:mainCRTStartup")
 #endif
 
-static void glfw_error_callback(int error, const char* description)
-{
-    fprintf(stderr, "Glfw Error %d: %s\n", error, description);
-}
+// Data
+static ID3D11Device*            g_pd3dDevice = NULL;
+static ID3D11DeviceContext*     g_pd3dDeviceContext = NULL;
+static IDXGISwapChain*          g_pSwapChain = NULL;
+static ID3D11RenderTargetView*  g_mainRenderTargetView = NULL;
+
+// Forward declarations of helper functions
+bool CreateDeviceD3D(HWND hWnd);
+void CleanupDeviceD3D();
+void CreateRenderTarget();
+void CleanupRenderTarget();
+LRESULT WINAPI WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam);
 
 namespace fs = std::filesystem;
 int run(const char *root_path);
 
-// tell compiler to not link as /SUBSYSTEM:console which adds an extra command prompt
-#pragma comment(linker, "/SUBSYSTEM:windows /ENTRY:mainCRTStartup")
-
-int main(int argc, char **argv) {
+// Main code
+int main(int argc, char **argv)
+{
     if (argc == 1) {
         std::cout << "Usage: main <root_dir>" << std::endl;
         spdlog::warn(fmt::format("Usage: main <root_dir>" ));
@@ -61,50 +66,46 @@ int main(int argc, char **argv) {
 }
 
 int run(const char *root_path) {
-    // Setup window
-    glfwSetErrorCallback(glfw_error_callback);
-    if (!glfwInit())
+    // Create application window
+    //ImGui_ImplWin32_EnableDpiAwareness();
+    WNDCLASSEX wc = { sizeof(WNDCLASSEX), CS_CLASSDC, WndProc, 0L, 0L, GetModuleHandle(NULL), NULL, NULL, NULL, NULL, _T("Torrent Renamer"), NULL };
+    ::RegisterClassEx(&wc);
+    HWND hwnd = ::CreateWindow(
+        wc.lpszClassName, 
+        _T("Torrent Renamer"), 
+        WS_OVERLAPPEDWINDOW, 
+        100, 100, 1280, 800, 
+        NULL, NULL, wc.hInstance, NULL);
+
+    // Initialize Direct3D
+    if (!CreateDeviceD3D(hwnd))
+    {
+        CleanupDeviceD3D();
+        ::UnregisterClass(wc.lpszClassName, wc.hInstance);
         return 1;
+    }
 
-    // Decide GL+GLSL versions
-#if defined(IMGUI_IMPL_OPENGL_ES2)
-    // GL ES 2.0 + GLSL 100
-    const char* glsl_version = "#version 100";
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 2);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
-    glfwWindowHint(GLFW_CLIENT_API, GLFW_OPENGL_ES_API);
-#elif defined(__APPLE__)
-    // GL 3.2 + GLSL 150
-    const char* glsl_version = "#version 150";
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
-    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);  // 3.2+ only
-    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);            // Required on Mac
-#else
-    // GL 3.0 + GLSL 130
-    const char* glsl_version = "#version 130";
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
-    //glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);  // 3.2+ only
-    //glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);            // 3.0+ only
-#endif
-
-    glfwWindowHint(GLFW_MAXIMIZED, GLFW_TRUE);
-    glfwWindowHint(GLFW_FOCUSED, GLFW_TRUE);
-
-    // Create window with graphics context
-    GLFWwindow* window = glfwCreateWindow(1280, 720, "TorrentRenamer", NULL, NULL);
-    if (window == NULL)
-        return 1;
-    glfwMakeContextCurrent(window);
-    glfwSwapInterval(1); // Enable vsync
+    // Show the window
+    ::ShowWindow(hwnd, SW_SHOWMAXIMIZED);
+    ::UpdateWindow(hwnd);
 
     // Setup Dear ImGui context
     IMGUI_CHECKVERSION();
     ImGui::CreateContext();
     ImGuiIO& io = ImGui::GetIO(); (void)io;
-    //io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;     // Enable Keyboard Controls
+    io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;       // Enable Keyboard Controls
     //io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;      // Enable Gamepad Controls
+    io.ConfigFlags |= ImGuiConfigFlags_DockingEnable;           // Enable Docking
+    io.ConfigFlags |= ImGuiConfigFlags_ViewportsEnable;         // Enable Multi-Viewport / Platform Windows
+    //io.ConfigViewportsNoAutoMerge = true;
+    //io.ConfigViewportsNoTaskBarIcon = true;
+    //io.ConfigViewportsNoDefaultParent = true;
+    //io.ConfigDockingAlwaysTabBar = true;
+    io.ConfigDockingTransparentPayload = true;
+    io.ConfigFlags |= ImGuiConfigFlags_DpiEnableScaleFonts;     // FIXME-DPI: Experimental. THIS CURRENTLY DOESN'T WORK AS EXPECTED. DON'T USE IN USER APP!
+    io.ConfigFlags |= ImGuiConfigFlags_DpiEnableScaleViewports; // FIXME-DPI: Experimental.
+
+    // load font
     io.Fonts->AddFontDefault();
     static const ImWchar icons_ranges[] = { ICON_MIN_FA, ICON_MAX_FA, 0 };
     ImFontConfig icons_config; 
@@ -117,18 +118,44 @@ int run(const char *root_path) {
     ImGui::StyleColorsLight();
     //ImGui::StyleColorsClassic();
 
+    // When viewports are enabled we tweak WindowRounding/WindowBg so platform windows can look identical to regular ones.
+    ImGuiStyle& style = ImGui::GetStyle();
+    if (io.ConfigFlags & ImGuiConfigFlags_ViewportsEnable)
+    {
+        style.WindowRounding = 0.0f;
+        style.Colors[ImGuiCol_WindowBg].w = 1.0f;
+    }
+
     // Setup Platform/Renderer backends
-    ImGui_ImplGlfw_InitForOpenGL(window, true);
-    ImGui_ImplOpenGL3_Init(glsl_version);
+    ImGui_ImplWin32_Init(hwnd);
+    ImGui_ImplDX11_Init(g_pd3dDevice, g_pd3dDeviceContext);
+
+    // Load Fonts
+    // - If no fonts are loaded, dear imgui will use the default font. You can also load multiple fonts and use ImGui::PushFont()/PopFont() to select them.
+    // - AddFontFromFileTTF() will return the ImFont* so you can store it if you need to select the font among multiple.
+    // - If the file cannot be loaded, the function will return NULL. Please handle those errors in your application (e.g. use an assertion, or display an error and quit).
+    // - The fonts will be rasterized at a given size (w/ oversampling) and stored into a texture when calling ImFontAtlas::Build()/GetTexDataAsXXXX(), which ImGui_ImplXXXX_NewFrame below will call.
+    // - Read 'docs/FONTS.md' for more instructions and details.
+    // - Remember that in C/C++ if you want to include a backslash \ in a string literal you need to write a double backslash \\ !
+    //io.Fonts->AddFontDefault();
+    //io.Fonts->AddFontFromFileTTF("../../misc/fonts/Roboto-Medium.ttf", 16.0f);
+    //io.Fonts->AddFontFromFileTTF("../../misc/fonts/Cousine-Regular.ttf", 15.0f);
+    //io.Fonts->AddFontFromFileTTF("../../misc/fonts/DroidSans.ttf", 16.0f);
+    //io.Fonts->AddFontFromFileTTF("../../misc/fonts/ProggyTiny.ttf", 10.0f);
+    //ImFont* font = io.Fonts->AddFontFromFileTTF("c:\\Windows\\Fonts\\ArialUni.ttf", 18.0f, NULL, io.Fonts->GetGlyphRangesJapanese());
+    //IM_ASSERT(font != NULL);
 
     // Our state
-    bool show_demo_window = false;
-#ifndef NDEBUG
-    show_demo_window = true;
-#endif
+    #ifdef NDEBUG
+    // TODO: enable dock space without demo window
+    bool show_demo_window = true;
+    #else
+    bool show_demo_window = true;
+    #endif
+
     ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
 
-
+    // create app after setting up the dx11 context
     app::App main_app("app_config.json");
 
     if (root_path == NULL) {
@@ -139,44 +166,156 @@ int run(const char *root_path) {
     }
 
     // Main loop
-    while (!glfwWindowShouldClose(window))
-    {
-        glfwPollEvents();
+    bool done = false;
+    while (!done) {
+        MSG msg;
+        while (::PeekMessage(&msg, NULL, 0U, 0U, PM_REMOVE)) {
+            ::TranslateMessage(&msg);
+            ::DispatchMessage(&msg);
+            if (msg.message == WM_QUIT) {
+                done = true;
+            }
+        }
+
+        if (done) {
+            break;
+        }
 
         // Start the Dear ImGui frame
-        ImGui_ImplOpenGL3_NewFrame();
-        ImGui_ImplGlfw_NewFrame();
+        ImGui_ImplDX11_NewFrame();
+        ImGui_ImplWin32_NewFrame();
         ImGui::NewFrame();
 
         // 1. Show the big demo window (Most of the sample code is in ImGui::ShowDemoWindow()! You can browse its code to learn more about Dear ImGui!).
-        if (show_demo_window)
-            ImGui::ShowDemoWindow(&show_demo_window);
-
-        // Render app
-        {
-            app::gui::RenderApp(main_app);
+        if (show_demo_window) {
+            ImGui::ShowDemoWindow();
         }
+
+        // render our app
+        app::gui::RenderApp(main_app);
 
         // Rendering
         ImGui::Render();
-        int display_w, display_h;
-        glfwGetFramebufferSize(window, &display_w, &display_h);
-        glViewport(0, 0, display_w, display_h);
-        glClearColor(clear_color.x * clear_color.w, clear_color.y * clear_color.w, clear_color.z * clear_color.w, clear_color.w);
-        glClear(GL_COLOR_BUFFER_BIT);
-        ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+        const float clear_color_with_alpha[4] = { clear_color.x * clear_color.w, clear_color.y * clear_color.w, clear_color.z * clear_color.w, clear_color.w };
+        g_pd3dDeviceContext->OMSetRenderTargets(1, &g_mainRenderTargetView, NULL);
+        g_pd3dDeviceContext->ClearRenderTargetView(g_mainRenderTargetView, clear_color_with_alpha);
+        ImGui_ImplDX11_RenderDrawData(ImGui::GetDrawData());
 
-        glfwSwapBuffers(window);
+        // Update and Render additional Platform Windows
+        if (io.ConfigFlags & ImGuiConfigFlags_ViewportsEnable) {
+            ImGui::UpdatePlatformWindows();
+            ImGui::RenderPlatformWindowsDefault();
+        }
+
+        g_pSwapChain->Present(1, 0); // Present with vsync
+        //g_pSwapChain->Present(0, 0); // Present without vsync
     }
 
     // Cleanup
-    ImGui_ImplOpenGL3_Shutdown();
-    ImGui_ImplGlfw_Shutdown();
+    ImGui_ImplDX11_Shutdown();
+    ImGui_ImplWin32_Shutdown();
     ImGui::DestroyContext();
 
-    glfwDestroyWindow(window);
-    glfwTerminate();
+    CleanupDeviceD3D();
+    ::DestroyWindow(hwnd);
+    ::UnregisterClass(wc.lpszClassName, wc.hInstance);
 
     return 0;
-
 }
+
+// Helper functions
+
+bool CreateDeviceD3D(HWND hWnd)
+{
+    // Setup swap chain
+    DXGI_SWAP_CHAIN_DESC sd;
+    ZeroMemory(&sd, sizeof(sd));
+    sd.BufferCount = 2;
+    sd.BufferDesc.Width = 0;
+    sd.BufferDesc.Height = 0;
+    sd.BufferDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM;
+    sd.BufferDesc.RefreshRate.Numerator = 60;
+    sd.BufferDesc.RefreshRate.Denominator = 1;
+    sd.Flags = DXGI_SWAP_CHAIN_FLAG_ALLOW_MODE_SWITCH;
+    sd.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT;
+    sd.OutputWindow = hWnd;
+    sd.SampleDesc.Count = 1;
+    sd.SampleDesc.Quality = 0;
+    sd.Windowed = TRUE;
+    sd.SwapEffect = DXGI_SWAP_EFFECT_DISCARD;
+
+    UINT createDeviceFlags = 0;
+    //createDeviceFlags |= D3D11_CREATE_DEVICE_DEBUG;
+    D3D_FEATURE_LEVEL featureLevel;
+    const D3D_FEATURE_LEVEL featureLevelArray[2] = { D3D_FEATURE_LEVEL_11_0, D3D_FEATURE_LEVEL_10_0, };
+    if (D3D11CreateDeviceAndSwapChain(NULL, D3D_DRIVER_TYPE_HARDWARE, NULL, createDeviceFlags, featureLevelArray, 2, D3D11_SDK_VERSION, &sd, &g_pSwapChain, &g_pd3dDevice, &featureLevel, &g_pd3dDeviceContext) != S_OK)
+        return false;
+
+    CreateRenderTarget();
+    return true;
+}
+
+void CleanupDeviceD3D()
+{
+    CleanupRenderTarget();
+    if (g_pSwapChain) { g_pSwapChain->Release(); g_pSwapChain = NULL; }
+    if (g_pd3dDeviceContext) { g_pd3dDeviceContext->Release(); g_pd3dDeviceContext = NULL; }
+    if (g_pd3dDevice) { g_pd3dDevice->Release(); g_pd3dDevice = NULL; }
+}
+
+void CreateRenderTarget()
+{
+    ID3D11Texture2D* pBackBuffer;
+    g_pSwapChain->GetBuffer(0, IID_PPV_ARGS(&pBackBuffer));
+    g_pd3dDevice->CreateRenderTargetView(pBackBuffer, NULL, &g_mainRenderTargetView);
+    pBackBuffer->Release();
+}
+
+void CleanupRenderTarget()
+{
+    if (g_mainRenderTargetView) { g_mainRenderTargetView->Release(); g_mainRenderTargetView = NULL; }
+}
+
+#ifndef WM_DPICHANGED
+#define WM_DPICHANGED 0x02E0 // From Windows SDK 8.1+ headers
+#endif
+
+// Forward declare message handler from imgui_impl_win32.cpp
+extern IMGUI_IMPL_API LRESULT ImGui_ImplWin32_WndProcHandler(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam);
+
+// Win32 message handler
+LRESULT WINAPI WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
+{
+    if (ImGui_ImplWin32_WndProcHandler(hWnd, msg, wParam, lParam))
+        return true;
+
+    switch (msg)
+    {
+    case WM_SIZE:
+        if (g_pd3dDevice != NULL && wParam != SIZE_MINIMIZED)
+        {
+            CleanupRenderTarget();
+            g_pSwapChain->ResizeBuffers(0, (UINT)LOWORD(lParam), (UINT)HIWORD(lParam), DXGI_FORMAT_UNKNOWN, 0);
+            CreateRenderTarget();
+        }
+        return 0;
+    case WM_SYSCOMMAND:
+        if ((wParam & 0xfff0) == SC_KEYMENU) // Disable ALT application menu
+            return 0;
+        break;
+    case WM_DESTROY:
+        ::PostQuitMessage(0);
+        return 0;
+    case WM_DPICHANGED:
+        if (ImGui::GetIO().ConfigFlags & ImGuiConfigFlags_DpiEnableScaleViewports)
+        {
+            //const int dpi = HIWORD(wParam);
+            //printf("WM_DPICHANGED to %d (%.0f%%)\n", dpi, (float)dpi / 96.0f * 100.0f);
+            const RECT* suggested_rect = (RECT*)lParam;
+            ::SetWindowPos(hWnd, NULL, suggested_rect->left, suggested_rect->top, suggested_rect->right - suggested_rect->left, suggested_rect->bottom - suggested_rect->top, SWP_NOZORDER | SWP_NOACTIVATE);
+        }
+        break;
+    }
+    return ::DefWindowProc(hWnd, msg, wParam, lParam);
+}
+
